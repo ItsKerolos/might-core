87c1f653c0dc1e2fd730e0681dbd63539082c116
Kerolos Zaki <52022280+ItsKerolos@users.noreply.github.com>
2020-07-28 07:36:51 +0200

    fix an export issue

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-export * from './utils.js';
+/**
+* @typedef { object } Step
+* @property {
+   'wait' |
+   'viewport' |
+   'select' |
+   'hover' |
+   'click' |
+   'type'
+  } action
+* @property { any } value
+*/
+
+export const actions = [
+  'wait',
+  'viewport',
+  'select',
+  'hover',
+  'click',
+  'type'
+];
+
+/**
+* @param { Step } step
+*/
+export function serializeStep(step)
+{
+  if (step.action === 'wait' && typeof step.value === 'number')
+    return `Wait ${step.value}s`;
+  
+  else if (step.action === 'wait')
+    return `Wait For ${step.value}`;
+
+  else if (step.action === 'viewport')
+    return `Viewport ${step.value}`;
+
+  else if (step.action === 'select')
+    return `Select ${step.value}`;
+  
+  else if (step.action === 'hover')
+    return 'Hover';
+
+  else if (step.action === 'click')
+    return 'Click';
+    
+  else if (step.action === 'type')
+    return `Type ${step.value}`;
+}
+
+/**
+* @param { Step[] } steps
+*/
+export function stepsToString(steps, separator)
+{
+  separator = separator || ' 🠮 ';
+
+  return steps.map(serializeStep).join(separator);
+}
