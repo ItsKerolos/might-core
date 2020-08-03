0b4405fabe3e37bcbacfc693c261f29c751bee72
Kerolos Zaki <52022280+ItsKerolos@users.noreply.github.com>
2020-08-03 03:28:04 +0200

    added new actions drag and swipe

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@
    'select' |
    'hover' |
    'click' |
+   'drag' |
+   'swipe' |
    'keyboard' |
    'type'
   } action
@@ -20,59 +22,130 @@ export const actions = [
   'select',
   'hover',
   'click',
+  'drag',
+  'swipe',
   'keyboard',
   'type'
 ];
 
 /**
 * @param { Step } step
+* @param { boolean } pretty
 */
-export function serializeStep(step)
+function serialize(step, pretty)
 {
+  /**
+  * @param { string } s
+  */
+  const cs = (s, pretty) => pretty ? s : s.toLowerCase();
+
   if (step.action === 'wait' && typeof step.value === 'number')
-    return `Wait ${step.value}s`;
+    return `${cs('Wait', pretty)} ${step.value}s`;
   
   else if (step.action === 'wait')
-    return `Wait For ${step.value}`;
+    return `${cs('Wait For', pretty)} ${step.value}`;
 
   else if (step.action === 'viewport')
-    return `Viewport ${step.value}`;
+    return `${cs('Viewport', pretty)} ${step.value}`;
 
   else if (step.action === 'media')
   {
-    const [ , value ] = step.value.split(':');
+    const [ name, value ] = step.value.split(':');
 
-    return `Media ${value.trim()}`;
+    let output;
+
+    if (pretty)
+      output = value;
+    else
+      output = `${name} ${value}`;
+
+    return `${cs('Media', pretty)} ${output}`;
   }
 
   else if (step.action === 'select')
-    return `Select ${step.value}`;
+    return `${cs('Select', pretty)} ${step.value}`;
   
   else if (step.action === 'hover')
-    return 'Hover';
+    return `${cs('Hover', pretty)}`;
 
   else if (step.action === 'click')
-    return 'Click';
+    return `${cs('Click', pretty)}`;
+
+  else if (step.action === 'drag')
+  {
+    const [ x1, y1 ] = step.value;
+
+    let output;
+
+    if (pretty)
+      output = '';
+    else
+      output = `${x1} ${y1}`;
+
+    return `${cs('Drag', pretty)} ${output}`;
+  }
+
+  else if (step.action === 'swipe')
+  {
+    let [ x0, y0, x1, y1 ] = step.value;
+
+    let output;
+
+    if (pretty)
+    {
+      x0 = parseInt(x0);
+      x1 = parseInt(x1);
+  
+      y0 = parseInt(y0);
+      y1 = parseInt(y1);
+
+      const xDiff = x0 - x1;
+      const yDiff = y0 - y1;
+  
+      if (xDiff > 0)
+        output = 'Left';
+      else if (xDiff < 0)
+        output = 'Right';
+
+      if (yDiff > 0)
+        output = output ? `Upper ${output}` : 'Up';
+      else if (yDiff < 0)
+        output = output ? `Lower ${output}` : 'Down';
+    }
+    else
+    {
+      output = `${x0} ${y0} ${x1} ${y1}`;
+    }
+
+    return `${cs('Swipe', pretty)} ${output}`;
+  }
 
   else if (step.action === 'keyboard')
-    return `Keyboard ${step.value}`;
+    return `${cs('Keyboard', pretty)} ${step.value}`;
     
   else if (step.action === 'type')
-    return `Type ${step.value}`;
+    return `${cs('Type', pretty)} ${step.value}`;
 
   else return undefined;
 }
 
 /**
-* @param { Step[] } steps
-* @param { string } separator
+* @param { Step } step
+* @param { boolean } pretty
 */
-export function stepsToString(steps, separator)
+export function serializeStep(step, pretty)
 {
-  separator = separator || ' 🠮 ';
+  return serialize(step, pretty).replace(/\s+/, ' ').trim();
+}
 
+/**
+* @param { Step[] } steps
+* @param { boolean } pretty
+*/
+export function stepsToString(steps, pretty)
+{
   return steps
-    .map(serializeStep)
+    .map(i => serializeStep(i, pretty))
     .filter(s => s !== undefined)
-    .join(separator);
+    .join(pretty ? ' 🠮 ' : '_');
 }
